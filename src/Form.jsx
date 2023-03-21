2f9b9d4a5646568134f182e6defc94a2fc9407a9
solidocean007 <clintonwilliams007@gmail.com>
2023-03-20 22:05:21 -0400

    finished

--- src/Form.jsx
+++ src/Form.jsx
@@ -19,8 +19,8 @@ function Form() {
     const userPayment = parseFloat(inputs.makePayment);
     const principal = parseFloat(inputs.totalDebt);
     const interestRate = parseFloat(inputs.interestRate) / 100;
-    return { userPayment, principal, interestRate};
-  }
+    return { userPayment, principal, interestRate };
+  };
 
   useEffect(() => {
     const { principal, interestRate } = getInputs();
@@ -32,8 +32,7 @@ function Form() {
   };
 
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
+    const { name, value } = event.target;
     setInputs((oldValues) => ({ ...oldValues, [name]: value }));
   };
 
@@ -56,7 +55,6 @@ function Form() {
 
   //
   const processPayment = (userPayment, principal, interestRate) => {
-
     const interest = calculateInterest(principal, interestRate);
     const principalPayment = userPayment - interest;
     const newBalance = principal - principalPayment;
@@ -65,6 +63,7 @@ function Form() {
     setPaymentHistory([
       ...paymentHistory,
       {
+        payment: userPayment,
         principal: principalPayment,
         interest: interest,
         remainingBalance: newBalance,
@@ -90,19 +89,17 @@ function Form() {
     }
   };
 
-  
-
   // Calculate payments remaining
   const calculatePaymentsRemaining = () => {
     const { userPayment, principal, interestRate } = getInputs();
-  
+
     if (userPayment <= 0) {
       return "N/A";
     }
-  
+
     let remainingBalance = principal;
     let paymentsRemaining = 0;
-  
+
     while (remainingBalance > 0) {
       const interest = calculateInterest(remainingBalance, interestRate);
       if (userPayment <= interest) {
@@ -112,13 +109,25 @@ function Form() {
       remainingBalance -= principalPayment;
       paymentsRemaining++;
     }
-  
+
     return paymentsRemaining;
   };
 
-  const principal = parseFloat(inputs.totalDebt) || 0;
-  const interestRate = parseFloat(inputs.interestRate) / 100 || 0;
-  const interest = calculateInterest(principal, interestRate);
+  const calculateTotalPrincipalPaid = () => {
+    return paymentHistory.reduce(
+      (accumulator, payment) => accumulator + payment.principal,
+      0
+    );
+  };
+
+  const calculateTotalInterestPaid = () => {
+    return paymentHistory.reduce(
+      (accumulator, payment) => accumulator + payment.interest,
+      0
+    );
+  };
+
+  const { principal, interestRate, interest, userPayment } = getInputs();
 
   return (
     <div className="Form">
@@ -126,55 +135,56 @@ function Form() {
       <form onSubmit={handleSubmit} action="">
         <div className="formInput">
           <div className="debtEntry">
-
             {/* Loan amount input */}
-            <label htmlFor="totalDebt">Loan amount</label>
-            <br />
-            <input
-              className="numInput"
-              name="totalDebt"
-              type="number"
-              value={inputs.totalDebt || ''}
-              id="totalDebt"
-              onChange={ handleChange }
-            />
-
-            <br />
-            <span>
-              Current Loan Amount: $
-              {parseFloat(inputs.totalDebt || 0).toFixed(2)}
-            </span>
-            <br />
+            <div className="entry">
+              <label htmlFor="totalDebt">Loan amount</label>
+              <br />
+              <input
+                className="numInput"
+                name="totalDebt"
+                type="number"
+                step="0.01"
+                // value={inputs.totalDebt || ""}
+                id="totalDebt"
+                onChange={handleChange}
+              />
+              <div>
+                Current Loan Amount: $
+                {parseFloat(inputs.totalDebt || 0).toFixed(2)}
+              </div>
+            </div>
 
             {/* Interest rate input */}
-            <label htmlFor="interestRate">Interest Rate</label>
-            <br />
-            <input
-              className="numInput"
-              name="interestRate"
-              type="number"
-              id="interestRate"
-              value={inputs.interestRate || ""}
-              onChange={handleChange}
-            />
-            <br />
+            <div className="entry">
+              <label htmlFor="interestRate">Interest Rate</label>
+              <br />
+              <input
+                className="numInput"
+                name="interestRate"
+                type="number"
+                id="interestRate"
+                value={inputs.interestRate || ""}
+                onChange={handleChange}
+              />
+            </div>
 
             {/* Make a payment field */}
-            <label htmlFor="makePayment">Make a payment</label>
-            <p>Minimum payment is: ${inputs.due.toFixed(2)}</p>
-            <br />
-            <input
-              className="numInput"
-              name="makePayment"
-              type="number"
-              id="makePayment"
-              value={inputs.makePayment || ''}
-              placeholder={inputs.due.toFixed(2)}
-              onChange={handleChange}
-            />
-            <br />
+            <div className="entry">
+              <label htmlFor="makePayment">Make a payment</label>
+              <p>Minimum payment is: ${inputs.due.toFixed(2)}</p>
 
-            <input type="submit" />
+              <input
+                className="numInput"
+                name="makePayment"
+                type="number"
+                id="makePayment"
+                value={inputs.makePayment || ""}
+                placeholder={inputs.due.toFixed(2)}
+                onChange={handleChange}
+              />
+
+              <input type="submit" />
+            </div>
           </div>
         </div>
 
@@ -183,28 +193,42 @@ function Form() {
           <h3>Monthly Payments</h3>
           <h1>${inputs.due.toFixed(2)}</h1>
           <div>
-            <h4>Interest : ${interest.toFixed(2)} per month.</h4>
-            <h4>Minimum principal is: ${calculateMinPrincipal(principal)} per month.</h4>
+            <h4>
+              Interest is: $
+              {calculateInterest(
+                parseFloat(inputs.totalDebt) || 0,
+                parseFloat(inputs.interestRate) / 100 || 0
+              ).toFixed(2)}{" "}
+              per month.
+            </h4>
+
+            <h4>
+              Minimum principal is: ${calculateMinPrincipal(principal)} per
+              month.
+            </h4>
           </div>
           <div>
             {paymentHistory.length > 0 && (
-              <h4>Total Principal Paid ${calculateMinPrincipal(principal)} for the entire loan.</h4>
+              <h4>
+                Total Principal Paid: $
+                {calculateTotalPrincipalPaid().toFixed(2)}
+              </h4>
             )}
             {paymentHistory.length > 0 && (
               <h4>
-                Total Interest Paid ${calculateInterest(interest)} for the entire loan.
+                Total Interest Paid: ${calculateTotalInterestPaid().toFixed(2)}
               </h4>
             )}
           </div>
         </div>
       </form>
 
-      <div className="paymentBlock">
-        <h3>{calculatePaymentsRemaining()} more payments to be debt free.</h3>
-        {console.log(calculatePaymentsRemaining + ' :cpr')}
-      </div>
-
-      <PaymentHistory paymentHistory={paymentHistory} />
+      {paymentHistory.length > 0 && (
+        <PaymentHistory
+          paymentHistory={paymentHistory}
+          calculatePaymentsRemaining={calculatePaymentsRemaining}
+        />
+      )}
 
       <MinAlert
         showAlert={showAlert}
--- src/PaymentHistory.jsx
+++ src/PaymentHistory.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-import './PaymentHistory.css';
+import "./PaymentHistory.css";
 
-const PaymentHistory = ({paymentHistory}) => {
+const PaymentHistory = ({ paymentHistory, calculatePaymentsRemaining }) => {
   return (
-    <div className="paymentHistory">
-    <h2>Payment History</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Payment</th>
-          <th>Principal</th>
-          <th>Interest</th>
-          <th>Remaining Balance</th>
-        </tr>
-      </thead>
-      <tbody>
-        {paymentHistory.map((payment, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>${payment.principal.toFixed(2)}</td>
-            <td>${payment.interest.toFixed(2)}</td>
-            <td>${payment.remainingBalance.toFixed(2)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+    <>
+      <div className="paymentBlock">
+        <h3>{calculatePaymentsRemaining()} more payments to be debt free.</h3>
+      </div>
+      <div className="paymentHistory">
+        <h2>Payment History</h2>
+        <table className="payment-table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Payment</th>
+              <th>Principal</th>
+              <th>Interest</th>
+              <th>Remaining Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paymentHistory.map((payment, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>${payment.payment.toFixed(2)}</td>
+                <td>${payment.principal.toFixed(2)}</td>
+                <td>${payment.interest.toFixed(2)}</td>
+                <td>${payment.remainingBalance.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
-  
 };
 
 PaymentHistory.defaultProps = {
   paymentHistory: [],
-}
-
+};
 
 export default PaymentHistory;
